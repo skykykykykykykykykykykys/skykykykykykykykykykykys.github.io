2b430f2ed40b7a9157b32812d7c9306b6f8eb5bc
Ismail F Aziz <ismail.bpn@gmail.com>
2024-07-02 23:09:57 +0700

    update nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  // other configurations...
+  app: {
+    baseURL: '/skykykykykykykykykykykys.github.io/'
+  },
+
+})
